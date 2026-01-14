0aef4e5b823dec399f8485453b201947fe796943
c0tton-fluff <145388770+c0tton-fluff@users.noreply.github.com>
2026-01-14 10:50:07 +0000

    Update Quartz config and scripts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "johnmatrix.org",
+    baseUrl: "https://johnmatrix.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
